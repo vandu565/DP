c8f2cfc92f6f14945c0c624a1c2c99bcad1bbdb3
Vandana Yadav <67888275+vandu565@users.noreply.github.com>
2020-10-10 13:13:16 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Longest Palindromic Subsequence.cpp
@@ -0,0 +1,29 @@
+ int longestPalindromeSubseq(string s) {
+        
+        string t=s;
+        reverse(s.begin(),s.end());
+        int len=s.size();
+        
+        int dp[len+1][len+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=1;i<=len;i++)
+        {
+            for(int j=1;j<=len;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    {
+                     dp[i][j]=1+dp[i-1][j-1];
+                    }
+                    else
+                    {
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    }
+                
+            }
+            
+        }
+        
+        return dp[len][len];
+    }
+};
